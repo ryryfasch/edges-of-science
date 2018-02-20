7e068de81d228deaf9d70739107008518ee56459
Nick Erokhin <nickerokhin@gmail.com>
2018-02-20 13:32:31 -0700

    getImages and makeImageSets done

--- remote-view.py
+++ remote-view.py
@@ -1,8 +1,28 @@
 import os
 import sys
-from PyQt4.QtGui import *
+#from PyQt4.QtGui import *
 import time
 import json
+import random
 
 
-data = json.load(open('cred.json'))
+#data = json.load(open('cred.json'))
+
+def getImages():
+
+    return os.listdir("./images/")
+
+
+def makeImageSets(images):
+    newImages = list(images)
+    random.shuffle(newImages)
+    random.shuffle(newImages)
+    upSet = newImages[0:15]
+    downSet = newImages[15:29]
+
+    return upSet, downSet
+
+#def selectRandomImage(ticker, upSet, downSet):
+#API call to ticker here
+
+print(makeImageSets(getImages()))
