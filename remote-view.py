d97bc9c0393176edc8a562375501a90dcd8becf2
ryryfasch <ryan.fasching@colorado.edu>
2018-03-13 20:47:40 -0600

    now uses two prices and compares them to get an image in the corresponfing set and cleared old code

--- remote-view.py
+++ remote-view.py
@@ -7,13 +7,6 @@ import random
 import sys
 import requests
 
-print(sys.argv[1])
-credit = json.load(open('cred.json', "r"))
-
-
-#https://api.iextrading.com/1.0/stock/snap/book?filter=lastSalePrice
-#params = {'function': 'TIME_SERIES_INTRADAY', 'symbol': 'MSFT', 'interval': '1min', 'apikey': credit["api_key"]}
-#print(credit)
 symbol = sys.argv[1]
 response = requests.get("https://api.iextrading.com/1.0/stock/{}/book?filter=lastSalePrice".format(symbol))
 response = response.json()
