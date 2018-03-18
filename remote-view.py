8ef584d6c2dcf0913d430dd3a481cacedfe4ee95
Nick Erokhin <nickerokhin@gmail.com>
2018-03-18 13:15:19 -0600

    fullscreen

--- remote-view.py
+++ remote-view.py
@@ -8,6 +8,32 @@ import sys
 import requests
 from PIL import Image
 
+if sys.version_info[0] == 2:  # the tkinter library changed it's name from Python 2 to 3.
+    import Tkinter
+    tkinter = Tkinter #I decided to use a library reference to avoid potential naming conflicts with people's programs.
+else:
+    import tkinter
+from PIL import Image, ImageTk
+
+def showPIL(pilImage):
+    root = tkinter.Tk()
+    w, h = root.winfo_screenwidth(), root.winfo_screenheight()
+    root.overrideredirect(1)
+    root.geometry("%dx%d+0+0" % (w, h))
+    root.focus_set()
+    root.bind("<Escape>", lambda e: (e.widget.withdraw(), e.widget.quit()))
+    canvas = tkinter.Canvas(root,width=w,height=h)
+    canvas.pack()
+    canvas.configure(background='black')
+    imgWidth, imgHeight = pilImage.size
+    if imgWidth > w or imgHeight > h:
+        ratio = min(w/imgWidth, h/imgHeight)
+        imgWidth = int(imgWidth*ratio)
+        imgHeight = int(imgHeight*ratio)
+        pilImage = pilImage.resize((imgWidth,imgHeight), Image.ANTIALIAS)
+    image = ImageTk.PhotoImage(pilImage)
+    imagesprite = canvas.create_image(w/2,h/2,image=image)
+    root.mainloop()
 
 
 
@@ -77,5 +103,5 @@ if __name__ == "__main__":
     print(newPrice)
     randImage = selectRandomImage(images[0], images[1], mostRecentPrice, newPrice)
     image = Image.open("./images/{}".format(randImage))
-    image.show()
+    showPIL(image)
     #print(randImage)
